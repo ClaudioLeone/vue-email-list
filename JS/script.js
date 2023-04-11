d2f2c7c7ec9cf036c0a81b8d33056f76f701c12c
ClaudioLeone <claleone92@gmail.com>
2023-04-12 01:01:50 +0200

    Implemented multiple random mail generations and html in-list injection [with bonus]

--- JS/script.js
+++ JS/script.js
@@ -3,16 +3,26 @@ const {createApp} = Vue;
 createApp ({
     data() {
         return {
-            rndMails: null
+            rndMails: null,
+            mailsArch: [],
         }
     },
 
     mounted() {
-        axios
-            .get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((response) => {
-                console.log(response.data.response);
-                this.rndMails = response.data.response;
-            })
+        this.getRndMail();
+    },
+
+    methods: {
+        getRndMail() {
+            for (let i = 0; i < 10; i++) {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((response) => {
+                    console.log(response.data.response);
+                    this.rndMails = response.data.response;
+                    this.mailsArch.push(this.rndMails);
+                })
+            }
+        }
     }
 }).mount("#app")
